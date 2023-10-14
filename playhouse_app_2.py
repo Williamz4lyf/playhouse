d51b1a016428400073dd1c2de99dc580725d682e
Nanke Williams <106990769+Williamz4lyf@users.noreply.github.com>
2023-10-14 07:16:28 +0100

    Update playhouse_app_2.py

--- playhouse_app_2.py
+++ playhouse_app_2.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import sklearn
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score, mean_squared_error
